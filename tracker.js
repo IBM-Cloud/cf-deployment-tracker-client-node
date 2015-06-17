4357e8747233b9c903f256154afc46af67835755
Jeff Sloyer <jbsloyer@us.ibm.com>
2015-06-17 11:38:08 -0400

    updated to be a module

--- tracker.js
+++ tracker.js
@@ -2,52 +2,44 @@
 
 var express = require("express"),
     restler = require("restler"),
-    path = require("path"),
-    program = require("commander"),
-    pkg = require(path.join(__dirname, "package.json"));
+    path = require("path");
 
-var vcapApplication;
+function track() {
+    var pkg = require(path.join(path.dirname(module.parent.filename), "package.json")),
+        vcapApplication;
 
-if (process.env.VCAP_APPLICATION) {
-    vcapApplication = JSON.parse(process.env.VCAP_APPLICATION);
+    if (process.env.VCAP_APPLICATION) {
+        vcapApplication = JSON.parse(process.env.VCAP_APPLICATION);
+    }
+
+    if (vcapApplication) {
+        var event = {
+            date_sent: new Date().toJSON()
+        };
+        if (pkg.version) {
+            event.code_version = pkg.version;
+        }
+        if (pkg.repository && pkg.repository.url) {
+            event.repository_url = pkg.repository.url;
+        }
+        if (vcapApplication.application_name) {
+            event.application_name = vcapApplication.application_name;
+        }
+        if (vcapApplication.space_id) {
+            event.space_id = vcapApplication.space_id;
+        }
+        if (vcapApplication.application_version) {
+            event.application_version = vcapApplication.application_version;
+        }
+        if (vcapApplication.application_uris) {
+            event.application_uris = vcapApplication.application_uris;
+        }
+        console.log(event);
+        var url = "https://deployment-tracker.mybluemix.net/api/v1/track";
+        restler.postJson(url, event).on("complete", function (data) {
+            console.log("Uplodaed stats");
+        });
+    }
 }
 
-program
-    .command("track")
-    .description("Track application deployments")
-    .action(function(options) {
-        if (vcapApplication) {
-            var event = {
-                date_sent: new Date().toJSON()
-            };
-            if (pkg.version) {
-                event.code_version = pkg.version;
-            }
-            if (pkg.repository && pkg.repository.url) {
-                event.repository_url = pkg.repository.url;
-            }
-            if (vcapApplication.application_name) {
-                event.application_name = vcapApplication.application_name;
-            }
-            if (vcapApplication.space_id) {
-                event.space_id = vcapApplication.space_id;
-            }
-            if (vcapApplication.application_version) {
-                event.application_version = vcapApplication.application_version;
-            }
-            if (vcapApplication.application_uris) {
-                event.application_uris = vcapApplication.application_uris;
-            }
-            var url = "https://deployment-tracker.mybluemix.net/";
-            restler.postJson(url, event).on("complete", function (data) {
-                console.log("Uplodaed stats");
-            });
-        }
-    }).on("--help", function() {
-        console.log("  Examples:");
-        console.log();
-        console.log("    $ track");
-        console.log();
-    });
-
-program.parse(process.argv);
+module.exports.track = track;
